e01b37613545f1837f03a942cea16128b33914a3
Joana Martins <joanalm@gmail.com>
2024-04-03 14:38:51 +0100

    Added original train and job_screation and workspace_preparation plus update to prep and utils

--- code/aml/src/prep/utils.py
+++ code/aml/src/prep/utils.py
@@ -6,6 +6,69 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
+# Utils functions
+import re
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+def getExistingProfileName(df, userId):
+    newProfileName = "Anonymous"
+    profileNames = df[df.UserId == userId].ProfileName.dropna()
+
+    if len(profileNames):
+        newProfileName = profileNames[0]
+
+    return newProfileName
+
+#Function to clean html tags from a sentence
+def removeHtml(sentence): 
+    pattern = re.compile('<.*?>')
+    cleaned_text = re.sub(pattern,' ',sentence)
+    return cleaned_text
+
+#Function to keep only words containing letters A-Z and a-z. This will remove all punctuations, special characters etc. https://stackoverflow.com/a/5843547/4084039
+def removePunctuations(sentence):
+    cleaned_text  = re.sub('[^a-zA-Z]',' ',sentence)
+    return (cleaned_text)
+
+#Remove words with numbers python: https://stackoverflow.com/a/18082370/4084039
+def removeNumbers(sentence):
+    sentence = re.sub("\S*\d\S*", " ", sentence).strip()
+    return (sentence)
+
+#Remove URL from sentences.
+def removeURL(sentence):
+    text = re.sub(r"http\S+", " ", sentence)
+    sentence = re.sub(r"www.\S+", " ", text)
+    return (sentence)
+
+#We will remove all such words which has three consecutive repeating characters.
+def removePatterns(sentence): 
+    cleaned_text  = re.sub("\\s*\\b(?=\\w*(\\w)\\1{2,})\\w*\\b",' ',sentence)
+    return (cleaned_text)
+
+#Expand the reviews x is aninput string of any length. Convert all the words to lower case
+def decontracted(x):
+    x = str(x).lower()
+    x = x.replace(",000,000", " m").replace(",000", " k").replace("′", "'").replace("’", "'")\
+                           .replace("won't", " will not").replace("cannot", " can not").replace("can't", " can not")\
+                           .replace("n't", " not").replace("what's", " what is").replace("it's", " it is")\
+                           .replace("'ve", " have").replace("'m", " am").replace("'re", " are")\
+                           .replace("he's", " he is").replace("she's", " she is").replace("'s", " own")\
+                           .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ")\
+                           .replace("€", " euro ").replace("'ll", " will").replace("how's"," how has").replace("y'all"," you all")\
+                           .replace("o'clock"," of the clock").replace("ne'er"," never").replace("let's"," let us")\
+                           .replace("finna"," fixing to").replace("gonna"," going to").replace("gimme"," give me").replace("gotta"," got to").replace("'d"," would")\
+                           .replace("daresn't"," dare not").replace("dasn't"," dare not").replace("e'er"," ever").replace("everyone's"," everyone is")\
+                           .replace("'cause'"," because")
+    
+    x = re.sub(r"([0-9]+)000000", r"\1m", x)
+    x = re.sub(r"([0-9]+)000", r"\1k", x)
+    return x
+
+
 ## Document vectorizer
 # vectorizer_type: tfidf if using TfidfVectorizer, doc2vec if using gensim doc2vec
 # train_series, val_series, test_series: pandas series for train/validation/test with text
