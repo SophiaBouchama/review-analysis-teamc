c34c89a3c3246acfe89e7aec2c510a480d29f9ab
Nohossat TRAORE <nohossat.traore@avanade.com>
2024-04-05 00:32:24 +0200

    finetuning lda

--- code/aml/src/train/train.py
+++ code/aml/src/train/train.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 from gensim.corpora import Dictionary
 from gensim.models import LdaModel
+from gensim.models import CoherenceModel
+
+import nltk
+from nltk.tokenize import word_tokenize
 
 import mlflow
 
@@ -26,8 +30,14 @@ mlflow.log_metric("nb of samples", df.shape[0])
 
 df.dropna(inplace=True)
 
+# Download the necessary NLTK data (if you haven't already)
+nltk.download('punkt')
+
+# Applying NLTK's tokenizer
+df['Token'] = df['CleanedText'].apply(lambda x: word_tokenize(x))
+
 # Convert the 'tokens' column into a list of lists for Gensim
-documents = df['CleanedText'].tolist()
+documents = df['Token'].tolist()
 
 # Create a Gensim Dictionary
 dictionary = Dictionary(documents)
@@ -44,5 +54,12 @@ lda_model = LdaModel(corpus=corpus, id2word=dictionary, num_topics=2, passes=10)
 # Explore the topics
 for idx, topic in lda_model.print_topics(-1):
     print(f"Topic: {idx} \nWords: {topic}")
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=corpus, dictionary=dictionary, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('Coherence Score: ', coherence_lda)
+
+mlflow.log_metric("coherence_score", coherence_lda)
 
 mlflow.save_model(lda_model, args.model_output)
