4c8d39d7b46f181f7bf7211f4e388c53a68b2970
Nohossat TRAORE <nohossat.traore@avanade.com>
2024-04-08 11:06:13 +0200

    fix pipeline

--- code/aml/naive-bayes/src/prep/test_prep.py
+++ code/aml/naive-bayes/src/prep/test_prep.py
@@ -1,8 +1,14 @@
-from utils import getExistingProfileName
-import pandas as pd
-import numpy as np
+from utils import removeHtml, removePunctuations, removeNumbers
 
-def test_answer():
-    data = {"UserId" : [1, 1, 1, 2, 3, 4], "ProfileName" : ["Bubbles", "Bubbles", np.nan, "Fox", "Bird", ""]}
-    df = pd.DataFrame(data)
-    assert getExistingProfileName(df, 1) == "Bubbles"
+
+def test_removeHTML():
+    data = "<p>Hello World</p>"
+    assert removeHtml(data) == " Hello World "
+
+def test_removePunctuations():
+    data = "Hello World!"
+    assert removePunctuations(data) == "Hello World "
+
+def test_removeNumbers():
+    data = "Hello World 123"
+    assert removeNumbers(data) == "Hello World"
